6ea1c67935959dcdf8c32aeaeb0ceacdc73a0d66
Ian Stride <ianstride@gmail.com>
2021-09-06 16:44:48 +0100

    Reduce number of global variables

--- scripts/create_excel_rows.js
+++ scripts/create_excel_rows.js
@@ -6,87 +6,92 @@ const [
     mapping_file_path,
     output_dir
 ] = process.argv.slice(2);
-const json_string = fs.readFileSync(input_file_path).toString();
-const obj_flows = JSON.parse(json_string);
 
+const obj_flows = JSON.parse(fs.readFileSync(input_file_path));
 const mapping_config = JSON.parse(fs.readFileSync(mapping_file_path));
+transform_to_excel_rows(obj_flows.flows, mapping_config);
 
-const files = obj_flows.flows.reduce(
-    (acc, flow) => {
-        const mapping = mapping_config.mappings.find(
-            (m) => flow.name.startsWith(m.flow_cat_name)
-        );
-        const file_name = mapping ? mapping.file_name : mapping_config.default_file_name;
-        if (acc[file_name]) {
-            acc[file_name].push(flow);
-        } else {
-            acc[file_name] = [flow];
-        }
-        return acc;
-    },
-    {}
-);
+function transform_to_excel_rows(flows, config) {
+    const files = groupFlows(flows, config);
 
-for (const [file_name, flows] of Object.entries(files)) {
+    for (const [file_name, flows] of Object.entries(files)) {
 
-    let flows_sheets = {};
+        let flows_sheets = {};
 
-    for (var flow of flows) {
-        console.log(flow.name);
+        for (const flow of flows) {
+            console.log(flow.name);
 
-        var nodes = flow.nodes;
-        addParents(nodes);
+            let nodes = addParents(flow.nodes);
+            let rows_obj = [];
+            let uuid_processed_nodes = [];
+            let node_waiting_list = [];
+            let uuid_waiting_list = [];
+            let curr_node = nodes[0];
+            let from_row_id = "start";
+            let row_counter = 1;
 
-        var rows_obj = [];
-        var uuid_processed_nodes = [];
-        var node_waiting_list = [];
-        var uuid_waiting_list = [];
-        var curr_node = nodes[0];
-        var from_row_id = "start";
-        var row_counter = 1;
+            while (uuid_waiting_list.length > 0 || nodes.length > uuid_processed_nodes.length) {
+                console.log("start while loop--------------------------------");
+                processNode(curr_node, from_row_id, uuid_processed_nodes, uuid_waiting_list, node_waiting_list, row_counter, rows_obj, flow, nodes);
 
-        while (uuid_waiting_list.length > 0 || nodes.length > uuid_processed_nodes.length) {
-            console.log("start while loop--------------------------------");
-            processNode(curr_node, from_row_id);
-
-            if (uuid_waiting_list.length > 0) {
-                curr_node = node_waiting_list[0];
-                console.log("processing first element in waiting list ------------------------------");
-                console.log(curr_node.uuid);
-                curr_node.unprocessed_parents = [];
-                from_row_id = null;
+                if (uuid_waiting_list.length > 0) {
+                    curr_node = node_waiting_list[0];
+                    console.log("processing first element in waiting list ------------------------------");
+                    console.log(curr_node.uuid);
+                    curr_node.unprocessed_parents = [];
+                    from_row_id = null;
+                }
             }
+            console.log("tot nodes: " + nodes.length + " processed: " + uuid_processed_nodes.length);
+
+            flows_sheets[flow.name] = JSON.parse(JSON.stringify(rows_obj));
         }
-        console.log("tot nodes: " + nodes.length + " processed: " + uuid_processed_nodes.length);
 
-        flows_sheets[flow.name] = JSON.parse(JSON.stringify(rows_obj));
+        flows_sheets = JSON.stringify(flows_sheets, null, 2);
+        const output_path = path.join(output_dir, `${file_name}.json`);
+        fs.writeFile(output_path, flows_sheets, (err, _) => {
+            if (err) console.log('error', err);
+        });
     }
-
-    flows_sheets = JSON.stringify(flows_sheets, null, 2);
-    const output_path = path.join(output_dir, `${file_name}.json`);
-    fs.writeFile(output_path, flows_sheets, (err, _) => {
-        if (err) console.log('error', err);
-    });
 }
 
-//////////////////////////////////////////////////////////////////////////////
-function addParents(nodes) {
-    for (nd = 0; nd < nodes.length; nd++) {
-        let node_uuid = nodes[nd].uuid;
-        nodes[nd].unprocessed_parents = [];
 
-        for (let o_nd = 0; o_nd < nodes.length; o_nd++) {
-            for (let ex = 0; ex < nodes[o_nd].exits.length; ex++) {
-                if (nodes[o_nd].exits[ex].destination_uuid == node_uuid) {
-                    nodes[nd].unprocessed_parents.push(nodes[o_nd].uuid);
+//////////////////////////////////////////////////////////////////////////////
+function groupFlows(flows, config) {
+    return flows.reduce(
+        (acc, flow) => {
+            const mapping = config.mappings.find(
+                (m) => flow.name.startsWith(m.flow_cat_name)
+            );
+            const file_name = mapping ? mapping.file_name : config.default_file_name;
+            if (acc[file_name]) {
+                acc[file_name].push(flow);
+            } else {
+                acc[file_name] = [flow];
+            }
+            return acc;
+        },
+        {}
+    );
+}
+
+function addParents(flow_nodes) {
+    for (let node of flow_nodes) {
+        node.unprocessed_parents = [];
+
+        for (let o_nd of flow_nodes) {
+            for (let ex of o_nd.exits) {
+                if (ex.destination_uuid == node.uuid) {
+                    node.unprocessed_parents.push(o_nd.uuid);
                 }
             }
         }
     }
+    return flow_nodes;
 }
 
 
-function processNode(curr_node, from_row_id) {
+function processNode(curr_node, from_row_id, uuid_processed_nodes, uuid_waiting_list, node_waiting_list, row_counter, rows_obj, flow, nodes) {
     console.log("start processnode---------------------------")
     console.log("Processed nodes " + uuid_processed_nodes)
     console.log("curr node id " + curr_node.uuid)
@@ -103,61 +108,50 @@ function processNode(curr_node, from_row_id) {
         // check if already processed
         if (uuid_processed_nodes.includes(curr_node.uuid)) {
             // create go_to row
-            var go_to_row = createGoToRow(curr_node, row_counter, from_row_id)
-            addConditions(curr_node, go_to_row)
+            let go_to_row = createGoToRow(curr_node, row_counter, from_row_id, rows_obj);
+            addConditions(curr_node, go_to_row, rows_obj, nodes);
             row_counter++;
-            console.log("print go_to row \n " + go_to_row)
+            console.log("print go_to row \n " + go_to_row);
 
         } else {
             // create the corresponding type of rows (one per action if there are multiple)
 
             if (curr_node.hasOwnProperty('router')) {
-                createRouterRow(curr_node, row_counter, from_row_id)
-
+                rows_obj.push(createRouterRow(curr_node, row_counter, from_row_id));
                 from_row_id = row_counter;
-                
                 row_counter++;
 
             } else {
                 curr_node.actions.forEach(action => {
+                    let row;
                     if (action.type == "send_msg") {
-                        createSendMsgRow(curr_node, action, row_counter, from_row_id);
-
+                        row = createSendMsgRow(curr_node, action, row_counter, from_row_id);
                     } else if (action.type == "set_contact_field") {
-                        createSaveValueRow(curr_node, action, row_counter, from_row_id);
-
-
+                        row = createSaveValueRow(curr_node, action, row_counter, from_row_id);
                     } else if (action.type == "add_contact_groups") {
-                        createAddToGroupRow(curr_node, action, row_counter, from_row_id);
-
+                        row = createAddToGroupRow(curr_node, action, row_counter, from_row_id);
                     } else if (action.type == "remove_contact_groups") {
-                        createRemoveFromGroupRow(curr_node, action, row_counter, from_row_id);
-
+                        row = createRemoveFromGroupRow(curr_node, action, row_counter, from_row_id);
                     } else if (action.type == "set_run_result") {
-                        createSaveFlowResultRow(curr_node, action, row_counter, from_row_id);
-
-
+                        row = createSaveFlowResultRow(curr_node, action, row_counter, from_row_id);
                     } else if (action.type == "set_contact_language") {
-                        createSetLanguageRow(curr_node, action, row_counter, from_row_id);
-
-
+                        row = createSetLanguageRow(curr_node, action, row_counter, from_row_id);
+                    } else {
+                        console.log('action not implemented');
                     }
-                    else {
-                        console.log("action not implemented")
+
+                    if (row) {
+                        rows_obj.push(row);
                     }
 
                     from_row_id = row_counter;
-                   
                     row_counter++;
                 });
-
-
-
             }
 
             curr_node.row_id = [];
             // rows created for the node
-            var node_rows = rows_obj.filter(r => (r._nodeId == curr_node.uuid));
+            let node_rows = rows_obj.filter(r => (r._nodeId == curr_node.uuid));
 
             // add _ui to row and save all row ids in rapidpro node
             node_rows.forEach(rw => {
@@ -167,11 +161,8 @@ function processNode(curr_node, from_row_id) {
             })
 
             // add conditions to the first row corresponding to the node
-            var first_row = node_rows.sort((r1, r2) => r1.row_id - r2.row_id)[0];
-            
-            addConditions(curr_node, first_row)
-
-
+            let first_row = node_rows.sort((r1, r2) => r1.row_id - r2.row_id)[0];
+            addConditions(curr_node, first_row, rows_obj, nodes);
 
             uuid_processed_nodes.push(curr_node.uuid)
             for (let ex = 0; ex < curr_node.exits.length; ex++) {
@@ -191,11 +182,8 @@ function processNode(curr_node, from_row_id) {
                     }
 
                     // call function recursively on next node
-                    processNode(next_node, from_row_id)
-
+                    processNode(next_node, from_row_id, uuid_processed_nodes, uuid_waiting_list, node_waiting_list, row_counter, rows_obj, flow, nodes);
                 }
-
-
             }
 
 
@@ -231,7 +219,7 @@ function processNode(curr_node, from_row_id) {
 
 function addFromRows(curr_node, curr_row, from_row_id) {
     curr_row.from = [];
-    var temp_from = [];
+    let temp_from = [];
     if (curr_node.hasOwnProperty("from_rows")) {
         temp_from = Array.from(curr_node.from_rows);
         curr_node["from_rows"] = [];
@@ -274,122 +262,85 @@ function createSendMsgRow(curr_node, curr_action, row_counter, from_row) {
             .replace("@(fields.animated_video_path & \"", "https://idems-media-recorder.web.app/storage/project/PLH/subproject/Rapidpro/deployment/Global/resourceGroup/animated/resourceType/video/eng/")
             .slice(0, -2);
 
-    })
+    });
     curr_row._nodeId = curr_node.uuid;
 
 
-    addFromRows(curr_node, curr_row, from_row)
+    addFromRows(curr_node, curr_row, from_row);
 
-    rows_obj.push(curr_row)
-
-
-    return
-
+    return curr_row;
 }
 
 function createSaveValueRow(curr_node, curr_action, row_counter, from_row) {
-    let curr_row = {};
-    curr_row.row_id = row_counter;
-    curr_row.type = "save_value";
+    let curr_row = {
+        row_id: row_counter,
+        type: 'save_value',
+        message_text: curr_action.value,
+        save_name: curr_action.field.name,
+        _nodeId: curr_node.uuid,
+    };
+    addFromRows(curr_node, curr_row, from_row);
 
-
-    curr_row.message_text = curr_action.value;
-    curr_row.save_name = curr_action.field.name;
-
-    curr_row._nodeId = curr_node.uuid;
-    addFromRows(curr_node, curr_row, from_row)
-
-
-    rows_obj.push(curr_row)
-
-
-    return
+    return curr_row;
 }
 
 function createAddToGroupRow(curr_node, curr_action, row_counter, from_row) {
-    let curr_row = {};
-    curr_row.row_id = row_counter;
-    curr_row.type = "add_to_group";
+    let curr_row = {
+        row_id: row_counter,
+        type: 'add_to_group',
+        message_text: curr_action.groups[0].name,
+        obj_id: curr_action.groups[0].uuid,
+        _nodeId: curr_node.uuid,
+    };
+    addFromRows(curr_node, curr_row, from_row);
 
-
-    curr_row.message_text = curr_action.groups[0].name;
-    curr_row.obj_id = curr_action.groups[0].uuid;
-
-    curr_row._nodeId = curr_node.uuid;
-
-    addFromRows(curr_node, curr_row, from_row)
-
-    rows_obj.push(curr_row)
-
-
-    return
+    return curr_row;
 }
 
 function createRemoveFromGroupRow(curr_node, curr_action, row_counter, from_row) {
-    let curr_row = {};
-    curr_row.row_id = row_counter;
-    curr_row.type = "remove_from_group";
+    let curr_row = {
+        row_id: row_counter,
+        type: 'remove_from_group',
+        message_text: curr_action.groups[0].name,
+        obj_id: curr_action.groups[0].uuid,
+        _nodeId: curr_node.uuid,
+    };
+    addFromRows(curr_node, curr_row, from_row);
 
-
-    curr_row.message_text = curr_action.groups[0].name;
-    curr_row.obj_id = curr_action.groups[0].uuid;
-
-    curr_row._nodeId = curr_node.uuid;
-
-    addFromRows(curr_node, curr_row, from_row)
-
-    rows_obj.push(curr_row)
-
-
-    return
+    return curr_row;
 }
 
-
 function createSaveFlowResultRow(curr_node, curr_action, row_counter, from_row) {
-    let curr_row = {};
-    curr_row.row_id = row_counter;
-    curr_row.type = "save_flow_result";
+    let curr_row = {
+        row_id: row_counter,
+        type: 'save_flow_result',
+        message_text: curr_action.value,
+        save_name: curr_action.name,
+        _nodeId: curr_node.uuid,
+    };
+    addFromRows(curr_node, curr_row, from_row);
 
-
-    curr_row.message_text = curr_action.value;
-    curr_row.save_name = curr_action.name;
-
-    curr_row._nodeId = curr_node.uuid;
-
-    addFromRows(curr_node, curr_row, from_row)
-
-    rows_obj.push(curr_row)
-
-
-    return
+    return curr_row;
 }
 
 function createSetLanguageRow(curr_node, curr_action, row_counter, from_row) {
+    let curr_row = {
+        row_id: row_counter,
+        type: 'set_language',
+        message_text: curr_action.language,
+        _nodeId: curr_node.uuid,
+    };
+    addFromRows(curr_node, curr_row, from_row);
 
-    let curr_row = {};
-    curr_row.row_id = row_counter;
-    curr_row.type = "set_language";
-
-
-    curr_row.message_text = curr_action.language;
-
-    curr_row._nodeId = curr_node.uuid;
-
-    addFromRows(curr_node, curr_row, from_row)
-
-    rows_obj.push(curr_row)
-
-
-    return
+    return curr_row;
 }
 
 
 function createRouterRow(curr_node, row_counter, from_row) {
-
     let curr_row = {};
     curr_row.row_id = row_counter;
     curr_row._nodeId = curr_node.uuid;
-    addFromRows(curr_node, curr_row, from_row)
+    addFromRows(curr_node, curr_row, from_row);
 
     if (curr_node.actions.length > 0) {
         // check if it's enter flow node
@@ -398,7 +349,7 @@ function createRouterRow(curr_node, row_counter, from_row) {
             curr_row.message_text = curr_node.actions[0].flow.name;
             curr_row.obj_id = curr_node.actions[0].flow.uuid;
         } else {
-            console.log("router node with action but not enter flow")
+            console.log('router node with action but not enter flow');
         }
     } else {
         // other split nodes
@@ -420,34 +371,31 @@ function createRouterRow(curr_node, row_counter, from_row) {
                 curr_row.message_text = curr_node.router.operand;
             }
         } else {
-            console.log("not recognised router type")
+            console.log('not recognised router type');
         }
-
-
     }
 
-    rows_obj.push(curr_row)
-
-
-    return
-
+    return curr_row;
 }
 
-function createGoToRow(curr_node, row_counter, from_row) {
-    let curr_row = {};
-    curr_row.row_id = row_counter;
-    curr_row.type = "go_to";
+function createGoToRow(curr_node, row_counter, from_row, rows_obj) {
+    let curr_row = {
+        row_id: row_counter,
+        type: 'go_to',
+        from: [from_row],
+    };
+    curr_row.message_text = Math.min(
+        rows_obj
+            .filter(row => row._nodeId == curr_node.uuid)
+            .map(row => row.row_id)
+    );
 
-    curr_row.from = [from_row];
-    curr_row.message_text = Math.min(rows_obj.filter(row => row._nodeId == curr_node.uuid).map(row => row.row_id));
-    rows_obj.push(curr_row)
-
-    return curr_row
+    return curr_row;
 }
 
 
 
-function addConditions(curr_node, curr_row) {
+function addConditions(curr_node, curr_row, rows_obj, nodes) {
     curr_row.condition = [];
     curr_row.condition_var = [];
     curr_row.condition_type = [];
@@ -457,7 +405,7 @@ function addConditions(curr_node, curr_row) {
     console.log(curr_row)
 
     // remove start from row.from
-    var from_list = JSON.parse(JSON.stringify(curr_row.from));
+    let from_list = JSON.parse(JSON.stringify(curr_row.from));
 
     let start_index = curr_row.from.indexOf("start");
     if (start_index > -1) {
